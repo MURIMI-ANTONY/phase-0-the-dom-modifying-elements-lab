cfdff19a5e5b6eb8c7467cccc6efec158ea08160
Murimi Antony <amurimi101@gmail.com>
2023-10-20 15:03:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the <main> element with id 'main'
+let mainElement = document.querySelector('main#main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+// Create a new <h1> element with id 'victory' and the text "YOUR-NAME is the champion"
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Murimi is the champion";
+
+// Append the newHeader to the body or another appropriate location in the DOM
+document.body.appendChild(newHeader);
+
